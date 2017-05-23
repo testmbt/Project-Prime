85620aab1686fc067ebb3c778d643b4f80d3c96b
testmbt <marc.bazil@gmail.com>
2017-05-23 16:54:20 -0500

    First Commit

--- ProjectA/src/com/app/Sale.java
+++ ProjectA/src/com/app/Sale.java
@@ -5,6 +5,7 @@ public class Sale {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Sale-1");
+         System.out.println("Sale-2");
 	}
 
 }
